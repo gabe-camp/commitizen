cgutierrezpa/jiratag-commitizen

feat(changelog): changelog tree generation from markdown

commitizen/changelog.py
@@ -1,0 +1,132 @@
+"""
+# DESIGN
+
+## Parse CHANGELOG.md
+
+1. Get LATEST VERSION in CHANGELOG
+1. Parse the file version to version
+2. Build a dict (tree) of that particular version
+3. Transform tree into markdown again
+
+## Parse git log
+
+1. get commits between versions
+2. filter commits with the current cz rules
+3. parse commit information
+4. generate tree
+
+Options:
+- Generate full or partial changelog
+"""
+from typing import Generator, List, Dict, Iterable
+import re
+
+MD_VERSION_RE = r"^##\s(?P<version>[a-zA-Z0-9.+]+)\s?\(?(?P<date>[0-9-]+)?\)?"
+MD_CATEGORY_RE = r"^###\s(?P<category>[a-zA-Z0-9.+\s]+)"
+MD_MESSAGE_RE = r"^-\s(\*{2}(?P<scope>[a-zA-Z0-9]+)\*{2}:\s)?(?P<message>.+)"
+md_version_c = re.compile(MD_VERSION_RE)
+md_category_c = re.compile(MD_CATEGORY_RE)
+md_message_c = re.compile(MD_MESSAGE_RE)
+
+
+CATEGORIES = [
+    ("fix", "fix"),
+    ("breaking", "BREAKING CHANGES"),
+    ("feat", "feat"),
+    ("refactor", "refactor"),
+    ("perf", "perf"),
+    ("test", "test"),
+    ("build", "build"),
+    ("ci", "ci"),
+    ("chore", "chore"),
+]
+
+
+def find_version_block(filepath: str) -> Generator:
+    """A block is understood as all the information about a version.
+
+    E.g:
+    ```
+    ## 1.2.1 (2019-07-20)
+
+    ## Bug fixes
+
+    - username validation not working
+
+    ## Features
+
+    - new login system
+
+    ```
+    """
+    with open(filepath, "r") as f:
+        block: list = []
+        for line in f:
+            line = line.strip("\n")
+            if not line:
+                continue
+
+            if line.startswith("## "):
+                if len(block) > 0:
+                    yield block
+                block = [line]
+            else:
+                block.append(line)
+        yield block
+
+
+def parse_md_version(md_version: str) -> Dict:
+    m = md_version_c.match(md_version)
+    if not m:
+        return {}
+    return m.groupdict()
+
+
+def parse_md_category(md_category: str) -> Dict:
+    m = md_category_c.match(md_category)
+    if not m:
+        return {}
+    return m.groupdict()
+
+
+def parse_md_message(md_message: str) -> Dict:
+    m = md_message_c.match(md_message)
+    if not m:
+        return {}
+    return m.groupdict()
+
+
+def transform_category(category: str) -> str:
+    _category_lower = category.lower()
+    for match_value, output in CATEGORIES:
+        if re.search(match_value, _category_lower):
+            return output
+    else:
+        raise ValueError(f"Could not match a category with {category}")
+
+
+def generate_block_tree(block: List[str]) -> Dict:
+    tree: Dict = {"commits": []}
+    category = None
+    for line in block:
+        if line.startswith("## "):
+            category = None
+            tree = {**tree, **parse_md_version(line)}
+        elif line.startswith("### "):
+            result = parse_md_category(line)
+            if not result:
+                continue
+            category = transform_category(result.get("category", ""))
+
+        elif line.startswith("- "):
+            commit = parse_md_message(line)
+            commit["category"] = category
+            tree["commits"].append(commit)
+        else:
+            print("it's something else: ", line)
+    return tree
+
+
+def generate_full_tree(blocks: Iterable) -> Iterable[Dict]:
+    for block in blocks:
+        yield generate_block_tree(block)

tests/test_changelog.py
@@ -1,0 +1,199 @@
+import os
+
+
+import pytest
+
+from commitizen import changelog
+
+
+COMMIT_LOG = [
+    "bump: version 1.5.0 → 1.5.1",
+    "",
+    "Merge pull request #29 from esciara/issue_28",
+    "fix: #28 allows poetry add on py36 envs",
+    "fix: #28 allows poetry add on py36 envs",
+    "",
+    "Merge pull request #26 from Woile/dependabot/pip/black-tw-19.3b0",
+    "chore(deps-dev): update black requirement from ^18.3-alpha.0 to ^19.3b0",
+    "Merge pull request #27 from Woile/dependabot/pip/mypy-tw-0.701",
+    "chore(deps-dev): update mypy requirement from ^0.700.0 to ^0.701",
+    "chore(deps-dev): update mypy requirement from ^0.700.0 to ^0.701",
+    "Updates the requirements on [mypy](https://github.com/python/mypy) to permit the latest version.",
+    "- [Release notes](https://github.com/python/mypy/releases)",
+    "- [Commits](https://github.com/python/mypy/compare/v0.700...v0.701)",
+    "",
+    "Signed-off-by: dependabot[bot] <support@dependabot.com>",
+    "chore(deps-dev): update black requirement from ^18.3-alpha.0 to ^19.3b0",
+    "Updates the requirements on [black](https://github.com/ambv/black) to permit the latest version.",
+    "- [Release notes](https://github.com/ambv/black/releases)",
+    "- [Commits](https://github.com/ambv/black/commits)",
+    "",
+    "Signed-off-by: dependabot[bot] <support@dependabot.com>",
+    "bump: version 1.4.0 → 1.5.0",
+    "",
+    "docs: add info about extra pattern in the files when bumping",
+    "",
+    "feat(bump): it is now possible to specify a pattern in the files attr to replace the version",
+    "",
+]
+
+CHANGELOG_TEMPLATE = """
+## 1.0.0 (2019-07-12)
+
+### Bug fixes
+
+- issue in poetry add preventing the installation in py36
+- **users**: lorem ipsum apap
+
+
+### Features
+
+- it is possible to specify a pattern to be matched in configuration files bump.
+
+## 0.9 (2019-07-11)
+
+### Bug fixes
+
+- holis
+
+"""
+
+
+@pytest.fixture
+def existing_changelog_file(request):
+    changelog_path = "tests/CHANGELOG.md"
+
+    with open(changelog_path, "w") as f:
+        f.write(CHANGELOG_TEMPLATE)
+
+    yield changelog_path
+
+    os.remove(changelog_path)
+
+
+def test_read_changelog_blocks(existing_changelog_file):
+    blocks = changelog.find_version_block(existing_changelog_file)
+    blocks = list(blocks)
+    amount_of_blocks = len(blocks)
+    assert amount_of_blocks == 2
+
+
+VERSION_CASES: list = [
+    ("## 1.0.0 (2019-07-12)", {"version": "1.0.0", "date": "2019-07-12"}),
+    ("## 2.3.0a0", {"version": "2.3.0a0", "date": None}),
+    ("## 0.10.0a0", {"version": "0.10.0a0", "date": None}),
+    ("## 1.0.0rc0", {"version": "1.0.0rc0", "date": None}),
+    ("## 1beta", {"version": "1beta", "date": None}),
+    (
+        "## 1.0.0rc1+e20d7b57f3eb (2019-3-24)",
+        {"version": "1.0.0rc1+e20d7b57f3eb", "date": "2019-3-24"},
+    ),
+    ("### Bug fixes", {}),
+    ("- issue in poetry add preventing the installation in py36", {}),
+]
+
+CATEGORIES_CASES: list = [
+    ("## 1.0.0 (2019-07-12)", {}),
+    ("## 2.3.0a0", {}),
+    ("### Bug fixes", {"category": "Bug fixes"}),
+    ("### Features", {"category": "Features"}),
+    ("- issue in poetry add preventing the installation in py36", {}),
+]
+CATEGORIES_TRANSFORMATIONS: list = [
+    ("Bug fixes", "fix"),
+    ("Features", "feat"),
+    ("BREAKING CHANGES", "BREAKING CHANGES"),
+]
+
+MESSAGES_CASES: list = [
+    ("## 1.0.0 (2019-07-12)", {}),
+    ("## 2.3.0a0", {}),
+    ("### Bug fixes", {}),
+    (
+        "- name no longer accept invalid chars",
+        {"message": "name no longer accept invalid chars", "scope": None},
+    ),
+    (
+        "- **users**: lorem ipsum apap",
+        {"message": "lorem ipsum apap", "scope": "users"},
+    ),
+]
+
+
+@pytest.mark.parametrize("test_input,expected", VERSION_CASES)
+def test_parse_md_version(test_input, expected):
+    assert changelog.parse_md_version(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", CATEGORIES_CASES)
+def test_parse_md_category(test_input, expected):
+    assert changelog.parse_md_category(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", CATEGORIES_TRANSFORMATIONS)
+def test_transform_category(test_input, expected):
+    assert changelog.transform_category(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", MESSAGES_CASES)
+def test_parse_md_message(test_input, expected):
+    assert changelog.parse_md_message(test_input) == expected
+
+
+def test_transform_category_fail():
+    with pytest.raises(ValueError) as excinfo:
+        changelog.transform_category("Bugs")
+    assert "Could not match a category" in str(excinfo.value)
+
+
+def test_generate_block_tree(existing_changelog_file):
+    blocks = changelog.find_version_block(existing_changelog_file)
+    block = next(blocks)
+    tree = changelog.generate_block_tree(block)
+    assert tree == {
+        "commits": [
+            {
+                "scope": None,
+                "message": "issue in poetry add preventing the installation in py36",
+                "category": "fix",
+            },
+            {"scope": "users", "message": "lorem ipsum apap", "category": "fix"},
+            {
+                "scope": None,
+                "message": "it is possible to specify a pattern to be matched in configuration files bump.",
+                "category": "feat",
+            },
+        ],
+        "version": "1.0.0",
+        "date": "2019-07-12",
+    }
+
+
+def test_generate_full_tree(existing_changelog_file):
+    blocks = changelog.find_version_block(existing_changelog_file)
+    tree = list(changelog.generate_full_tree(blocks))
+
+    assert tree == [
+        {
+            "commits": [
+                {
+                    "scope": None,
+                    "message": "issue in poetry add preventing the installation in py36",
+                    "category": "fix",
+                },
+                {"scope": "users", "message": "lorem ipsum apap", "category": "fix"},
+                {
+                    "scope": None,
+                    "message": "it is possible to specify a pattern to be matched in configuration files bump.",
+                    "category": "feat",
+                },
+            ],
+            "version": "1.0.0",
+            "date": "2019-07-12",
+        },
+        {
+            "commits": [{"scope": None, "message": "holis", "category": "fix"}],
+            "version": "0.9",
+            "date": "2019-07-11",
+        },
+    ]

gabe-camp/commitizen

commitizen/changelog.py
@@ -1,0 +1,133 @@
+"""
+# DESIGN
+
+## Parse CHANGELOG.md
+
+1. Get LATEST VERSION from CONFIG
+1. Parse the file version to version
+2. Build a dict (tree) of that particular version
+3. Transform tree into markdown again
+
+## Parse git log
+
+1. get commits between versions
+2. filter commits with the current cz rules
+3. parse commit information
+4. generate tree
+
+Options:
+- Generate full or partial changelog
+"""
+from typing import Generator, List, Dict, Iterable
+import re
+
+MD_VERSION_RE = r"^##\s(?P<version>[a-zA-Z0-9.+]+)\s?\(?(?P<date>[0-9-]+)?\)?"
+MD_CATEGORY_RE = r"^###\s(?P<category>[a-zA-Z0-9.+\s]+)"
+MD_MESSAGE_RE = r"^-\s(\*{2}(?P<scope>[a-zA-Z0-9]+)\*{2}:\s)?(?P<message>.+)"
+md_version_c = re.compile(MD_VERSION_RE)
+md_category_c = re.compile(MD_CATEGORY_RE)
+md_message_c = re.compile(MD_MESSAGE_RE)
+
+
+CATEGORIES = [
+    ("fix", "fix"),
+    ("breaking", "BREAKING CHANGES"),
+    ("feat", "feat"),
+    ("refactor", "refactor"),
+    ("perf", "perf"),
+    ("test", "test"),
+    ("build", "build"),
+    ("ci", "ci"),
+    ("chore", "chore"),
+]
+
+
+def find_version_blocks(filepath: str) -> Generator:
+    """
+    version block: contains all the information about a version.
+
+    E.g:
+    ```
+    ## 1.2.1 (2019-07-20)
+
+    ## Bug fixes
+
+    - username validation not working
+
+    ## Features
+
+    - new login system
+
+    ```
+    """
+    with open(filepath, "r") as f:
+        block: list = []
+        for line in f:
+            line = line.strip("\n")
+            if not line:
+                continue
+
+            if line.startswith("## "):
+                if len(block) > 0:
+                    yield block
+                block = [line]
+            else:
+                block.append(line)
+        yield block
+
+
+def parse_md_version(md_version: str) -> Dict:
+    m = md_version_c.match(md_version)
+    if not m:
+        return {}
+    return m.groupdict()
+
+
+def parse_md_category(md_category: str) -> Dict:
+    m = md_category_c.match(md_category)
+    if not m:
+        return {}
+    return m.groupdict()
+
+
+def parse_md_message(md_message: str) -> Dict:
+    m = md_message_c.match(md_message)
+    if not m:
+        return {}
+    return m.groupdict()
+
+
+def transform_category(category: str) -> str:
+    _category_lower = category.lower()
+    for match_value, output in CATEGORIES:
+        if re.search(match_value, _category_lower):
+            return output
+    else:
+        raise ValueError(f"Could not match a category with {category}")
+
+
+def generate_block_tree(block: List[str]) -> Dict:
+    tree: Dict = {"commits": []}
+    category = None
+    for line in block:
+        if line.startswith("## "):
+            category = None
+            tree = {**tree, **parse_md_version(line)}
+        elif line.startswith("### "):
+            result = parse_md_category(line)
+            if not result:
+                continue
+            category = transform_category(result.get("category", ""))
+
+        elif line.startswith("- "):
+            commit = parse_md_message(line)
+            commit["category"] = category
+            tree["commits"].append(commit)
+        else:
+            print("it's something else: ", line)
+    return tree
+
+
+def generate_full_tree(blocks: Iterable) -> Iterable[Dict]:
+    for block in blocks:
+        yield generate_block_tree(block)

tests/test_changelog.py
@@ -1,0 +1,199 @@
+import os
+
+
+import pytest
+
+from commitizen import changelog
+
+
+COMMIT_LOG = [
+    "bump: version 1.5.0 → 1.5.1",
+    "",
+    "Merge pull request #29 from esciara/issue_28",
+    "fix: #28 allows poetry add on py36 envs",
+    "fix: #28 allows poetry add on py36 envs",
+    "",
+    "Merge pull request #26 from Woile/dependabot/pip/black-tw-19.3b0",
+    "chore(deps-dev): update black requirement from ^18.3-alpha.0 to ^19.3b0",
+    "Merge pull request #27 from Woile/dependabot/pip/mypy-tw-0.701",
+    "chore(deps-dev): update mypy requirement from ^0.700.0 to ^0.701",
+    "chore(deps-dev): update mypy requirement from ^0.700.0 to ^0.701",
+    "Updates the requirements on [mypy](https://github.com/python/mypy) to permit the latest version.",
+    "- [Release notes](https://github.com/python/mypy/releases)",
+    "- [Commits](https://github.com/python/mypy/compare/v0.700...v0.701)",
+    "",
+    "Signed-off-by: dependabot[bot] <support@dependabot.com>",
+    "chore(deps-dev): update black requirement from ^18.3-alpha.0 to ^19.3b0",
+    "Updates the requirements on [black](https://github.com/ambv/black) to permit the latest version.",
+    "- [Release notes](https://github.com/ambv/black/releases)",
+    "- [Commits](https://github.com/ambv/black/commits)",
+    "",
+    "Signed-off-by: dependabot[bot] <support@dependabot.com>",
+    "bump: version 1.4.0 → 1.5.0",
+    "",
+    "docs: add info about extra pattern in the files when bumping",
+    "",
+    "feat(bump): it is now possible to specify a pattern in the files attr to replace the version",
+    "",
+]
+
+CHANGELOG_TEMPLATE = """
+## 1.0.0 (2019-07-12)
+
+### Bug fixes
+
+- issue in poetry add preventing the installation in py36
+- **users**: lorem ipsum apap
+
+
+### Features
+
+- it is possible to specify a pattern to be matched in configuration files bump.
+
+## 0.9 (2019-07-11)
+
+### Bug fixes
+
+- holis
+
+"""
+
+
+@pytest.fixture
+def existing_changelog_file(request):
+    changelog_path = "tests/CHANGELOG.md"
+
+    with open(changelog_path, "w") as f:
+        f.write(CHANGELOG_TEMPLATE)
+
+    yield changelog_path
+
+    os.remove(changelog_path)
+
+
+def test_read_changelog_blocks(existing_changelog_file):
+    blocks = changelog.find_version_blocks(existing_changelog_file)
+    blocks = list(blocks)
+    amount_of_blocks = len(blocks)
+    assert amount_of_blocks == 2
+
+
+VERSION_CASES: list = [
+    ("## 1.0.0 (2019-07-12)", {"version": "1.0.0", "date": "2019-07-12"}),
+    ("## 2.3.0a0", {"version": "2.3.0a0", "date": None}),
+    ("## 0.10.0a0", {"version": "0.10.0a0", "date": None}),
+    ("## 1.0.0rc0", {"version": "1.0.0rc0", "date": None}),
+    ("## 1beta", {"version": "1beta", "date": None}),
+    (
+        "## 1.0.0rc1+e20d7b57f3eb (2019-3-24)",
+        {"version": "1.0.0rc1+e20d7b57f3eb", "date": "2019-3-24"},
+    ),
+    ("### Bug fixes", {}),
+    ("- issue in poetry add preventing the installation in py36", {}),
+]
+
+CATEGORIES_CASES: list = [
+    ("## 1.0.0 (2019-07-12)", {}),
+    ("## 2.3.0a0", {}),
+    ("### Bug fixes", {"category": "Bug fixes"}),
+    ("### Features", {"category": "Features"}),
+    ("- issue in poetry add preventing the installation in py36", {}),
+]
+CATEGORIES_TRANSFORMATIONS: list = [
+    ("Bug fixes", "fix"),
+    ("Features", "feat"),
+    ("BREAKING CHANGES", "BREAKING CHANGES"),
+]
+
+MESSAGES_CASES: list = [
+    ("## 1.0.0 (2019-07-12)", {}),
+    ("## 2.3.0a0", {}),
+    ("### Bug fixes", {}),
+    (
+        "- name no longer accept invalid chars",
+        {"message": "name no longer accept invalid chars", "scope": None},
+    ),
+    (
+        "- **users**: lorem ipsum apap",
+        {"message": "lorem ipsum apap", "scope": "users"},
+    ),
+]
+
+
+@pytest.mark.parametrize("test_input,expected", VERSION_CASES)
+def test_parse_md_version(test_input, expected):
+    assert changelog.parse_md_version(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", CATEGORIES_CASES)
+def test_parse_md_category(test_input, expected):
+    assert changelog.parse_md_category(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", CATEGORIES_TRANSFORMATIONS)
+def test_transform_category(test_input, expected):
+    assert changelog.transform_category(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", MESSAGES_CASES)
+def test_parse_md_message(test_input, expected):
+    assert changelog.parse_md_message(test_input) == expected
+
+
+def test_transform_category_fail():
+    with pytest.raises(ValueError) as excinfo:
+        changelog.transform_category("Bugs")
+    assert "Could not match a category" in str(excinfo.value)
+
+
+def test_generate_block_tree(existing_changelog_file):
+    blocks = changelog.find_version_blocks(existing_changelog_file)
+    block = next(blocks)
+    tree = changelog.generate_block_tree(block)
+    assert tree == {
+        "commits": [
+            {
+                "scope": None,
+                "message": "issue in poetry add preventing the installation in py36",
+                "category": "fix",
+            },
+            {"scope": "users", "message": "lorem ipsum apap", "category": "fix"},
+            {
+                "scope": None,
+                "message": "it is possible to specify a pattern to be matched in configuration files bump.",
+                "category": "feat",
+            },
+        ],
+        "version": "1.0.0",
+        "date": "2019-07-12",
+    }
+
+
+def test_generate_full_tree(existing_changelog_file):
+    blocks = changelog.find_version_blocks(existing_changelog_file)
+    tree = list(changelog.generate_full_tree(blocks))
+
+    assert tree == [
+        {
+            "commits": [
+                {
+                    "scope": None,
+                    "message": "issue in poetry add preventing the installation in py36",
+                    "category": "fix",
+                },
+                {"scope": "users", "message": "lorem ipsum apap", "category": "fix"},
+                {
+                    "scope": None,
+                    "message": "it is possible to specify a pattern to be matched in configuration files bump.",
+                    "category": "feat",
+                },
+            ],
+            "version": "1.0.0",
+            "date": "2019-07-12",
+        },
+        {
+            "commits": [{"scope": None, "message": "holis", "category": "fix"}],
+            "version": "0.9",
+            "date": "2019-07-11",
+        },
+    ]